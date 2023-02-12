0cfb1439dded385ccec489fd5639a0486a74e8db
Ksusha1809 <Lka1809@mail.ru>
2023-02-12 14:41:23 +0300

    63-69

---
+++ Task67/Program.cs
@@ -0,0 +1,15 @@
+﻿//Задание 67. Напишиту программу, которая будет принимать на вход число и
+//возвращать сумму его цифр.
+//453->12
+//45->9
+
+Console.WriteLine("Введите  число");
+int num = Convert.ToInt32(Console.ReadLine());
+int sumNumber = SumNumber(num);
+Console.WriteLine($"Сумма цифр числа равна {sumNumber}");
+
+int SumNumber (int number)
+{
+    if (number == 0) return 0;
+    else return number % 10 +SumNumber(number / 10);
+}
---
+++ Task69/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача 69: Напишите программу, которая на вход принимает два числа А и В, и
+//возводит А в целею степент В с помощью ркурсии. 
+
+Console.WriteLine("Введите  число A");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите  число B");
+int num2 = Convert.ToInt32(Console.ReadLine());
+int power = PowerNumber(num1, num2);
+
+Console.WriteLine($"{num1} в степени {num2} равно {power}");
+
+int PowerNumber(int number1, int number2)
+{
+    if (number2== 0) return 1;
+    else return number1 * PowerNumber(number1, number2 - 1);
+}
